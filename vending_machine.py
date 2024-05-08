1a8847430840d15ff26ec6c973a56965355b6d38
faruklmu17 <41293321+faruklmu17@users.noreply.github.com>
2024-05-08 11:26:30 -0400

    Create vending_machine.py

---
+++ vending_machine.py
@@ -0,0 +1,8 @@
+#this is a python application
+import variables 
+print("Welcome to X's Vending Machine")
+print("Product  Code    Price")
+print("Coke     1001    $"+str(variables.coke_price))
+print("Mars     1002    $"+str(variables.mars_price))
+print("Pepsi    1003    $"+str(variables.pepsi_price))
+print("Doritos  1004    $"+str(variables.doritos_price))
